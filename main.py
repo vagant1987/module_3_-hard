48305b1907545c0ee7bba5b34f08c7d8a57c5151
vagant1987 <vogant87@mail.ru>
2024-11-26 21:08:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ data_structure = [
     [1, 2, 3],
     {'a': 4, 'b': 5},
     (6, {'cube': 7, 'drum': 8}),
-    "Hello", ((), [{(2, 'Urban', ('Urban2', 35))}])
-]
+    "Hello", ((), [{(2, 'Urban', ('Urban2', 35))}])]
 
 def calculate_structure_sum(data_structure):
     sum_numbers = 0
